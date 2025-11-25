3ef24433179afcba4751895ab538af14d8b0f3cb
Abhishek7Tech <abhishektab45@gmail.com>
2025-11-25 14:49:12 +0530

    implement heading section and add dummy data for profile

---
+++ app/utils/profileList.ts
@@ -0,0 +1,24 @@
+const Profile = [
+  {
+    name: "About app",
+    heading: "About Us",
+    content: "This a clone project.",
+    type: "modal",
+  },
+  {
+    name: "Terms & Conditions",
+    heading: "Terms of Use for YAP Clone",
+    content: "This a clone project.",
+    type: "modal",
+  },
+  {
+    name: "Privacy Policy for YAP Clone",
+    heading: "Terms of Use for YAP Clone",
+    content: "This a clone project.",
+    type: "modal",
+  },
+  {
+    name: "Report an Issue",
+    type: "redirect",
+  },
+];
